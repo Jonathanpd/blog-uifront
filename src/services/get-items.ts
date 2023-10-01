ca81f8b40e8361b98ad778724abd32e61838f439
Jonathan PD - Jonathan Santos <34203527+Jonathanpd@users.noreply.github.com>
2023-10-01 14:59:47 -0300

    feat: BASE_API_URL | .env

--- src/services/get-items.ts
+++ src/services/get-items.ts
@@ -1,16 +1,17 @@
 import { cache } from 'react'
 
 const revalidateNumber = 60 // Revalidate every 60 seconds
+const baseApiUrl = process.env.BASE_API_URL;
 
 export const getItems = cache(async () => { // Feed Posts
-  const items = await fetch(`http://apiuifront.local/json/api/article/`, {
+  const items = await fetch(`${baseApiUrl}/article/`, {
     next: { revalidate: revalidateNumber },
   }).then((res) => res.json())
   return items
 })
 
 export const getItem = cache(async (slug: string) => { // Single Post
-  const item = await fetch(`http://apiuifront.local/json/api/article/${slug}`, {
+  const item = await fetch(`${baseApiUrl}/article/${slug}`, {
     next: { revalidate: revalidateNumber },
   }).then((res) => res.json())
   return item
