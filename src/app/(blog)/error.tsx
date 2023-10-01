566ab3f78351360986c8383f3c3c92ce59e52e1d
Jonathan PD - Jonathan Santos <34203527+Jonathanpd@users.noreply.github.com>
2023-10-01 17:19:05 -0300

    fix: text erros

--- src/app/(blog)/error.tsx
+++ src/app/(blog)/error.tsx
@@ -16,7 +16,7 @@ export default function Error({
  
   return (
     <div>
-      <h2>Something went wrong!</h2>
+      <h2>Error Blog | Something went wrong!</h2>
       <button
         onClick={
           // Attempt to recover by trying to re-render the segment
