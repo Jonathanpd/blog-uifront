7f7b2417821e8f93953bba1edd5bf58642a43f59
Jonathan PD - Jonathan Santos <34203527+Jonathanpd@users.noreply.github.com>
2023-10-01 16:20:00 -0300

    feat: shadcn ui | init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { ChevronRightIcon } from "@radix-ui/react-icons"
+import { Button } from '@/components/ui/button';
 import { getItems } from '@/services/get-items'
 import Link from 'next/link';
 
@@ -17,6 +19,11 @@ export default async function Home() {
             <h1>{post.title}</h1>
             <h2>{post.slug}</h2>
           </Link>
+          <Button variant="ghost" asChild>
+            <Link href={`/${post.slug}`}>
+              Ver Post <ChevronRightIcon className="h-4 w-4" />
+            </Link>
+          </Button>
         </div>
       ))}
     </main>
