eb9a8b17bac7bf498ae86799c01346fff7997bae
Jonathan PD - Jonathan Santos <34203527+Jonathanpd@users.noreply.github.com>
2023-10-07 22:26:20 -0300

    feat: first story | component: Hero

---
+++ src/components/Sections/Hero/stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import Hero from '.'
+
+export default {
+  title: 'Sections/Hero',
+  component: Hero
+} as Meta
+
+export const Default: StoryObj = {}
