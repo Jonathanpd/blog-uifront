d8b67ae3c20d7b7209ac79baec8a8c1a3fb33663
Jonathan PD - Jonathan Santos <34203527+Jonathanpd@users.noreply.github.com>
2023-10-07 22:29:56 -0300

    feat: first story | component: Hero | tailwind css | preview storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from '@storybook/react'
 
+import '../node_modules/tailwindcss/tailwind.css'
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
