970a9f519d368ae3caaca28c59b0447429b204b2
Jonathan PD - Jonathan Santos <34203527+Jonathanpd@users.noreply.github.com>
2024-03-24 18:24:47 -0300

    feat: textos atualizados | hero home blog

--- src/components/Sections/Hero/index.tsx
+++ src/components/Sections/Hero/index.tsx
@@ -8,8 +8,8 @@ type HeroProps = {
 
 export default function Hero({
   className,
-  title = 'Bem-vindo ao Blog Ui & Front',
-  description = 'Explore nossos artigos sobre elementos de interface, desde o UX / Ui Design até o desenvolvimento Front-end, para o sucesso do seu projeto.'
+  title = 'Bem-vindo ao Blog Ui & Front!',
+  description = 'Descobra dicas práticas e insights valiosos sobre design de interfaces e desenvolvimento front-end, com ênfase em: estilo, funcionalidades e propósito.' //description = 'Descubra uma variedade de conteúdos em nosso blog, abrangendo desde os princípios do design UX/UI até o desenvolvimento Front-end, essenciais para impulsionar o sucesso do seu projeto. Explore agora!'
 }: HeroProps) {
   const combinedClassName = `bg-white ${className}`
 
@@ -31,7 +31,7 @@ export default function Hero({
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Explorando o Melhor do UI Design & Front-End.{' '}
+              Mantenha-se atualizado sobre UX/UI Design & Front-End Engineering.{' '}
               <a href="#" className="font-semibold text-indigo-600">
                 <span className="absolute inset-0" aria-hidden="true" />
                 Não perca nada <span aria-hidden="true">&rarr;</span>
@@ -47,16 +47,16 @@ export default function Hero({
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
-                href="/artigos"
+                href="/tutoriais"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Ver Artigos
+                Explorar Tutoriais
               </Link>
               <Link
-                href="#"
+                href="/artigos"
                 className="text-sm font-semibold leading-6 text-gray-900"
               >
-                Ver Tutoriais <span aria-hidden="true">→</span>
+                Explorar Artigos <span aria-hidden="true">→</span>
               </Link>
             </div>
           </div>
