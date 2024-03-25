abd8424f4a51002403ff1feaf9f52b845059b5ee
Jonathan PD - Jonathan Santos <34203527+Jonathanpd@users.noreply.github.com>
2024-03-25 12:55:41 -0300

    fix: tag | front-end para ui ux design

--- src/components/Sections/BlogSection/BlogSection.tsx
+++ src/components/Sections/BlogSection/BlogSection.tsx
@@ -82,7 +82,7 @@ export async function BlogGrid({ className, maxPost }: BlogGridProps) {
                     {post.date}
                   </time>
                   <span className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
-                    Front-end
+                    UX / UI Design
                   </span>
                 </CardDescription>
 
