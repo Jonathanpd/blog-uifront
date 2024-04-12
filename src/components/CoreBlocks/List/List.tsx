c24217d355eaff24baa8402a2c41d37b48a14ff2
Jonathan PD - Jonathan Santos <34203527+Jonathanpd@users.noreply.github.com>
2024-04-11 22:35:09 -0300

    fix: Text content does not match server-rendered HTML | react-hydration-error

--- src/components/CoreBlocks/List/List.tsx
+++ src/components/CoreBlocks/List/List.tsx
@@ -1,23 +1,31 @@
-import { FC } from 'react'
+'use client'
+
+import { FC, useState, useEffect } from 'react'
 import { Block } from '@/utils/types/blockTypes'
 
 interface ListProps {
   innerBlocks?: Block[]
-  block: Block
 }
 
-export const List: FC<ListProps> = ({ innerBlocks = [], block }) => {
+export const List: FC<ListProps> = ({ innerBlocks = [] }) => {
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
   return (
     <ul className="list-disc my-5">
-      {innerBlocks.map((item, index) => {
-        return (
-          <li
-            key={index}
-            dangerouslySetInnerHTML={{ __html: block.listItems[index] }}
-            className={`font-list max-w-5xl mx-auto my-1 text-base leading-8`}
-          />
-        )
-      })}
+      {isClient &&
+        innerBlocks.map((item, index) => {
+          return (
+            <li
+              key={index}
+              dangerouslySetInnerHTML={{ __html: item.innerHTML }}
+              className={`font-list max-w-5xl mx-auto my-1 text-base leading-8`}
+            />
+          )
+        })}
     </ul>
   )
 }
--- src/components/CoreBlocks/Paragraph/Paragraph.jsx
+++ src/components/CoreBlocks/Paragraph/Paragraph.jsx
@@ -1,12 +1,25 @@
+'use client'
+
+import { useState, useEffect } from 'react'
 import { getTextAlign } from '@/utils/fonts'
 
 export const Paragraph = ({ textAlign = 'left', content = '' }) => {
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
   return (
-    <p
-      dangerouslySetInnerHTML={{ __html: content }}
-      className={`font-paragraph max-w-5xl mx-auto my-5 text-base leading-8 ${getTextAlign(
-        textAlign
-      )}`}
-    />
+    <>
+      {isClient && (
+        <p
+          dangerouslySetInnerHTML={{ __html: content }}
+          className={`font-paragraph max-w-5xl mx-auto my-5 text-base leading-8 ${getTextAlign(
+            textAlign
+          )}`}
+        />
+      )}
+    </>
   )
 }
